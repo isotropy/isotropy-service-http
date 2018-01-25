9881dba30263425a395b49eb648f11337b448319
jeswin <jeswinpk@agilehead.com>
2018-01-25 19:01:27 +0530

    WIP: Does not work

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
-export async function run() {
-  
+export interface ServiceConfig {
+  name: string;
+  nodes?: number;
+  type: string;
+  modules: string[];
+}
+
+export interface HttpServiceLocation {
+  type: string;
+  location: string;
+}
+
+export interface HttpServiceNodeJSLocation extends HttpServiceLocation {
+  type: "nodejs";
+  main: string;
+}
+
+export interface HttpServiceStaticLocation extends HttpServiceLocation {
+  type: "static";
+  path: string;
+}
+
+export interface HttpServiceConfig extends ServiceConfig {
+  locations: HttpServiceLocation[];
+}
+
+async function handleProxyLocation() {
+
+}
+
+async function handleStaticLocation() {
+
+}
+
+export async function run(config: HttpServiceConfig) {
+  for (const location of config.locations) {
+    
+  }
 }
